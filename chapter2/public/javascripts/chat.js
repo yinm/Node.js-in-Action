59a3b6ddaa1c0f345e874e51d85d3ff69f0c70a9
yinm <yinm.cminor9@gmail.com>
2017-06-18 17:06:10 +0900

    チャットコマンドの処理

--- chapter2/public/javascripts/chat.js
+++ chapter2/public/javascripts/chat.js
@@ -15,3 +15,29 @@ Chat.prototype.changeRoom = function(room) {
     newRoom: room
   });
 };
+
+Chat.prototype.processCommand = function(command) {
+  const words = command.split(' ');
+  const command = words[0]
+                  .substring(1, words[0].length)
+                  .toLowerCase();
+  let message = false;
+
+  switch(command) {
+    case 'join':
+      words.shift();
+      const room = words.join(' ');
+      this.changeRoom(room);
+      break;
+    case 'nick':
+      words.shift();
+      const name = words.join(' ');
+      this.socket.emit('nameAttempt', name);
+      break;
+    default:
+      message = 'Unrecognized command.';
+      break;
+  };
+
+  return message;
+};
