8a079ea38bcfcd7aa780aced43cf336f0b6d2cc9
yinm <yinm.cminor9@gmail.com>
2017-06-18 17:32:49 +0900

    Socket.IOのイベントを初期化する

--- chapter2/public/javascripts/chat_ui.js
+++ chapter2/public/javascripts/chat_ui.js
@@ -23,3 +23,57 @@ function processUserInput(chatApp, socket) {
 
   $('#send-message').val('');
 }
+
+const socket = io.connect = io.connect();
+
+$(document).ready(function() {
+  const chatApp = new Chat(socket);
+
+  socket.on('nameResult', function(result) {
+    let message;
+
+    if (result.success) {
+      message = 'You are now known as ' + result.name  '.';
+    } else {
+      message = result.message;
+    }
+    $('#messages').append(divSystemContentElement(message));
+  });
+
+  socket.on('joinResult', function(result) {
+    $('#room').text(result.room);
+    $('#messages').append(divSystemContentElement('Room changed.'));
+  });
+
+  socket.on('message', function(message) {
+    const newElement = $('<div></div>').text(message.text);
+    $('#messages').append(newElement);
+  });
+
+  socket.on('rooms', function(rooms) {
+    $('#room-list').empty();
+
+    for (let room in rooms) {
+      room = room.substring(1, room.length);
+      if (room != '') {
+        $('#room-list').append(divEscapeContentElement(room));
+      }
+    }
+
+    $('#room-list div').click(function() {
+      chatApp.processCommand('/join ' + $(this).text());
+      $('#send-message').focus();
+    });
+  });
+
+  setInterval(function() {
+    socket.emit('rooms');
+  }, 1000);
+
+  $('#send-message').focus();
+
+  $('#send-form').submit(function() {
+    processUserInput(chatApp, socket);
+    return false;
+  });
+});
