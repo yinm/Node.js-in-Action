9e5c9c0c85d37d35bb318be4ae093da407000698
yinm <yinm.cminor9@gmail.com>
2017-06-18 16:15:23 +0900

    ゲスト名の割り当て

--- chapter2/lib/chat_server.js
+++ chapter2/lib/chat_server.js
@@ -26,3 +26,14 @@ exports.listen = function(server) {
     handleClientDisconnection(socket, nickNames, namesUsed);
   });
 };
+
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+  const name = 'Guest' + guestNumber;
+  nickNames[socket.id] = name;
+  socket.emit('nameResult', {
+    success: true,
+    name: name
+  });
+  namesUsed.push(name);
+  return guestNumber + 1;
+}
