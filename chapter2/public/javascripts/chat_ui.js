724ad3b2a56134b4b1c46de23384eb49a5dd39cb
yinm <yinm.cminor9@gmail.com>
2017-06-18 17:17:35 +0900

    rawデータの処理

--- chapter2/public/javascripts/chat_ui.js
+++ chapter2/public/javascripts/chat_ui.js
@@ -5,3 +5,21 @@ function divEscapeContentElement(message) {
 function divSystemContentElement(message) {
   return $('<div></div>').html('<i>' + message + '</i>');
 }
+
+function processUserInput(chatApp, socket) {
+  const message = $('#send-message').val();
+  let systemMessage;
+
+  if (message.chatAt(0) == '/') {
+    systemMessage = chatApp.processCommand(message);
+    if (systemMessage) {
+      $('#messages').append(divSystemContentElement(message));
+    }
+  } else {
+    chatApp.sendMEssage($('#room').text(), message);
+    $('#messages').append(divEscapeContentElement(message));
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  }
+
+  $('#send-message').val('');
+}
