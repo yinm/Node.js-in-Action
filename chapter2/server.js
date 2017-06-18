caa31866f4ef65e9191319b3343932a5c2ad16ff
yinm <yinm.cminor9@gmail.com>
2017-06-18 10:43:20 +0900

    404エラーを返す

--- chapter2/server.js
+++ chapter2/server.js
@@ -6,3 +6,9 @@ const
 
 let
   cache = {};
+
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
