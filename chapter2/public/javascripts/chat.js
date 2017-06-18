5ab92cc5d33672d6fd618fba80f1d1b499af075c
yinm <yinm.cminor9@gmail.com>
2017-06-18 16:55:44 +0900

    メッセージ送信

--- chapter2/public/javascripts/chat.js
+++ chapter2/public/javascripts/chat.js
@@ -1,3 +1,11 @@
 const Chat = function(socket) {
   this.socket = socket;
 };
+
+Chat.prototype.sendMessage = function(room, text) {
+  const message = {
+    room: room,
+    text: text
+  };
+  this.socket.emit('message', message);
+};
