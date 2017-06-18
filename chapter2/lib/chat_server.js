0edd103fd25763a2b553b3dfab1ba5a6175d39f3
yinm <yinm.cminor9@gmail.com>
2017-06-18 16:09:44 +0900

    Socket.IOのサーバーを始動する

--- chapter2/lib/chat_server.js
+++ chapter2/lib/chat_server.js
@@ -1,10 +1,28 @@
 const
-  socketio = require('socket.io'),
-  guestNumber = 1;
+  socketio = require('socket.io');
 
 let
   io,
+  guestNumber = 1,
   nickNames = {},
   namesUsed = [],
   currentRoom = {};
 
+exports.listen = function(server) {
+  io = socketio.listen(server);
+  io.set('log level', 1);
+
+  io.sockets.on('connection', function(socket) {
+    guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+    joinRoom(socket, 'Lobby');
+
+    handleMessageBroadcasting(socket, nickNames);
+    handleNameChangeAttempts(socket, nickNames, namesUsed);
+    handleRoomJoining(socket);
+
+    socket.on('rooms', function() {
+      socket.emit('rooms', io.sockets.manager.rooms);
+    });
+    handleClientDisconnection(socket, nickNames, namesUsed);
+  });
+};
