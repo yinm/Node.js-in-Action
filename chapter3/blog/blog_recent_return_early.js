2b288452ece9b563c9ecf2972a2f0d9d72446028
yinm <yinm.cminor9@gmail.com>
2017-06-20 08:17:26 +0900

    早期リターンを使って、ネストを浅くする

---
+++ chapter3/blog/blog_recent_return_early.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(req, res) {
+  getTitles(res);
+}).listen(8000, '127.0.0.1');
+
+console.log('server listening...');
+
+function getTitles(res) {
+  fs.readFile('./title.json', function(err, data) {
+    if (err) return hadError(err, res)
+
+    getTemplate(JSON.parse(data.toString()), res);
+  })
+}
+
+function getTemplate(titles, res) {
+  fs.readFile('./template.html', function(err, data) {
+    if (err) return hadError(err, res);
+    formatHtml(titles, data.toString(), res);
+  })
+}
+
+function formatHtml(titles, tmpl, res) {
+  const html = tmpl.replace('%', titles.join('</li><li>'));
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(html);
+}
+
+function hadError(err, res) {
+  console.error(err);
+  res.end('Server Error');
+}
