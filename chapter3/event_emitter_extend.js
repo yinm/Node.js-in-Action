3341398efe6cae001254e86cb15acb7ef3ec22a9
yinm <yinm.cminor9@gmail.com>
2017-06-21 21:40:50 +0900

    イベントエミッタの機能拡張

--- chapter3/event_emitter_extend.js
+++ chapter3/event_emitter_extend.js
@@ -7,3 +7,25 @@ const events = require('events');
 const util = require('util');
 
 util.inherits(Watcher, events.EventEmitter);
+
+const fs = require('fs');
+const watchDir = './watch';
+const processeDir = './done';
+
+Watcher.prototype.watch = function() {
+  const watcher = this;
+  fs.readDir(this.watchDir, function(err, files) {
+    if (err) throw err;
+
+    for (index in files) {
+      watcher.emit('process', files[index]);
+    }
+  })
+};
+
+Watcher.prototype.start = function() {
+  const watcher = this;
+  fs.watchFile(watchDir, function() {
+    watcher.watch();
+  });
+};
